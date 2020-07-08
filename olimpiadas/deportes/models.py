41a538a59064b237ee53a8b88ad3dcb53e7411d3
Mauricio Tellez <mauricio@solucioni.mx>
2020-07-07 22:57:31 -0500

    Commit inicial.

---
+++ olimpiadas/deportes/models.py
@@ -0,0 +1,42 @@
+from django.db import models
+
+
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=64, unique=True)
+
+
+class Deporte(models.Model):
+    nombre = models.CharField(max_length=64, unique=True)
+    categoria = models.ForeignKey(Categoria,
+                                  on_delete=models.CASCADE,
+                                  default=1)
+
+
+class Estado(models.Model):
+    nombre = models.CharField(max_length=32, unique=True)
+    abreviado = models.CharField(max_length=4)
+
+
+class Escuela(models.Model):
+    nombre = models.CharField(max_length=128, unique=True)
+    estado = models.ForeignKey(Estado, on_delete=models.CASCADE)
+    ciudad = models.CharField(max_length=64)
+    direccion = models.CharField(max_length=128)
+    cp = models.CharField(max_length=5)
+
+
+class Sede(models.Model):
+    nombre = models.CharField(max_length=128, unique=True)
+    escuela = models.ForeignKey(Escuela, on_delete=models.CASCADE)
+    estado = models.ForeignKey(Estado, on_delete=models.CASCADE)
+    ciudad = models.CharField(max_length=64)
+    direccion = models.CharField(max_length=128)
+    cp = models.CharField(max_length=5)
+
+
+class Competencia(models.Model):
+    nombre = models.CharField(max_length=128)
+    escuela = models.ForeignKey(Escuela, on_delete=models.CASCADE)
+    sede = models.ManyToManyField(Sede)
+    fecha_inicio = models.DateField()
+    fecha_final = models.DateField()
